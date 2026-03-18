38081b5ec847461ed13dfab0178a681ff4454369
Retears <lspelech@gmail.com>
2026-03-18 13:00:03 +0100

    tarea 1

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-emerald-800">
       <h1>Lista de tareas</h1>
+      <h1>hola linda</h1>
       {
         listaTareas.map(tareaContenido => <Tarea key={tareaContenido.id} tarea={tareaContenido}/>)
       }
